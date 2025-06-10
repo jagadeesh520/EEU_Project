25091ec015f3be4fc155bfb7bb9b29648f87e740
Ashokkumar Balakrishnan <ashok@Ashokkumars-MacBook-Air.local>
2025-06-10 14:50:10 +0530

    Updated: common component screen

--- src/CommonComponent/CommonComponent.js
+++ src/CommonComponent/CommonComponent.js
@@ -22,11 +22,7 @@ const CommonHeader = ({ navigation, title, onBackPress }) => {
       </View>
     );
   };
-<<<<<<< HEAD
 
-  return (
-    <View style={[styles.CommonHeaderMain, { alignItems: 'flex-start' }]}>
-=======
   console.log(title, "title-text amharic")
   return (
     <View>
@@ -36,25 +32,19 @@ const CommonHeader = ({ navigation, title, onBackPress }) => {
       </View>
     <View style={styles.CommonHeaderMain}>
      <View style={{ display: 'flex', flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
->>>>>>> development
       <View style={styles.CommonHeaderBackBtn}>
         <TouchableOpacity onPress={onBackPress}>
           <Image source={ImagePath.LeftArrow} />
         </TouchableOpacity>
         <Text style={styles.CommonHeaderTxt}>{t(title)}</Text>
       </View> 
-<<<<<<< HEAD
-      <View style={[styles.LanguageContainer, { flexDirection: 'column', display: 'flex', alignItems: 'flex-end', justifyContent: 'flex-end' }]}>
-        <MultipleOption navigation={navigation} />
-         <Dropdown
-=======
+
       <View style={[styles.LanguageContainer]}>
         <MultipleOption navigation={navigation} />
       </View>  
       </View> 
       <View style={{ display: 'flex', flexDirection: 'row', flex:1, justifyContent: 'flex-end'}}>
         <Dropdown
->>>>>>> development
              placeholderStyle={styles.RaiseComplaintDropdownTxt}
              selectedTextStyle={styles.RaiseComplaintDropdownTxt}
              inputSearchStyle={styles.RaiseComplaintDropdownTxt}
@@ -62,11 +52,7 @@ const CommonHeader = ({ navigation, title, onBackPress }) => {
              labelField="label"
              valueField="value"
              placeholder={t("Language")}
-<<<<<<< HEAD
-             style={[styles.LanguageDropdown, { marginTop: -10 }]}
-=======
              style={[styles.LanguageDropdown]}
->>>>>>> development
              renderItem={renderItems}
              data={languageOption}
              value={selectedLang}
@@ -74,12 +60,6 @@ const CommonHeader = ({ navigation, title, onBackPress }) => {
                setSelectedLang(item.value);
                i18n.changeLanguage(item.value)
              }}               
-<<<<<<< HEAD
-        />
-      </View>  
-      </View> 
-     
-=======
           />
           { title === "Service Request Status" || title === "የአገልግሎት ጥያቄ ሁኔታ" ? 
           <TouchableOpacity
@@ -97,8 +77,6 @@ const CommonHeader = ({ navigation, title, onBackPress }) => {
         </View> 
     
       </View>
-
->>>>>>> development
   );
 };
 
